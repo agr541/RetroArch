4c648d7f8ba20c06ec1bad06c6da69bbdd2b65ec
seducinglife <leea@seducinglife.com>
2026-06-26 05:56:40 +0200

    fix

--- pkg/android/phoenix-common/jni/Application.mk
+++ pkg/android/phoenix-common/jni/Application.mk
@@ -12,19 +12,19 @@ else
    endif
 endif
 
+
 ifndef TARGET_ABIS
-   APP_ABI := armeabi-v7a mips x86
+   APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 else
    APP_ABI := $(TARGET_ABIS)
 endif
 
-# Run $NDK/toolchains/llvm-3.6/prebuilt/linux-x86_64/bin/asan_device_setup once
-# for each AVD you use, replacing llvm-3.6 with the latest you have and
-# linux-x86_64 with your host ABI.
+
+# AddressSanitizer block
 ifneq ($(SANITIZER),)
    # AddressSanitizer doesn't run on mips
    ifndef TARGET_ABIS
-      APP_ABI   := armeabi-v7a x86
+      APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
    else
       ifneq ($(findstring mips,$(TARGET_ABIS)),)
          $(error "AddressSanitizer does not support mips.")
@@ -33,10 +33,12 @@ ifneq ($(SANITIZER),)
    USE_CLANG := 1
 endif
 
+
 ifeq ($(USE_CLANG),1)
    NDK_TOOLCHAIN_VERSION := clang
    APP_CFLAGS   := -Wno-invalid-source-encoding
    APP_CPPFLAGS := -Wno-invalid-source-encoding
 endif
+
 
 APP_STL := c++_static
